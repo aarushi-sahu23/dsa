49f7b83c967ee41125ba5654b8369ec27b2e378f
aarushi-sahu23 <128160061+aarushi-sahu23@users.noreply.github.com>
2024-06-10 23:55:40 +0530

    Time: 2 ms (48.33%), Space: 10.2 MB (10.73%) - LeetHub

---
+++ 1137-height-checker/1137-height-checker.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> expected = heights;
+        
+        // Sort the expected array
+        sort(expected.begin(), expected.end());
+        
+        // Count the number of indices where heights[i] != expected[i]
+        int count = 0;
+        for (size_t i = 0; i < heights.size(); ++i) {
+            if (heights[i] != expected[i]) {
+                ++count;
+            }
+        }
+        
+        return count;
+    }
+};
